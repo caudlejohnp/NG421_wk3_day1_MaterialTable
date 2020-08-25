83a6eca94bb027a89b80e2ecc31895d81e75c317
caudlejohnp <caudlejohnp@gmail.com>
2020-08-25 12:44:54 -0500

    added mat table module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { IGroup } from './interface/igroup';
+import { GroupService } from './services/group.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MaterialTable';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['playerName', 'charName', 'race', 'class', 'level'];
+  dataSource: MatTableDataSource<IGroup>;
+
+  constructor(private groupService: GroupService) {  }
+
+  ngOnInit(): void { 
+    this.dataSource = new MatTableDataSource(this.groupService.getMembers());
+  }
 }
